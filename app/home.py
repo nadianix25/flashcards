b3fe86f75efa26c062ef76aec04b69b4101bee31
winninix25 <mimix25@gmail.com>
2022-09-05 15:05:52 +0100

    add select

--- app/home.py
+++ app/home.py
@@ -9,7 +9,10 @@ from app.db import get_db
 
 bp = Blueprint('home', __name__, url_prefix='/home')
 
+def get_cards():
+    return get_db().execute('SELECT * FROM card').fetchall()
+
 @bp.route('/', methods=(['GET']))
 def home():
-
-    return render_template('home/dashboard.html')
+    cards = get_cards()
+    return render_template('home/dashboard.html', cards=cards)
