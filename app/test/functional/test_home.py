94b15897c91218bbfc82478dfaa1d7aac9de48cf
nadia.nobre <nadiamsnobre@gmail.com>
2022-09-09 13:25:40 +0100

    correcting home test

--- app/test/functional/test_home.py
+++ app/test/functional/test_home.py
@@ -4,9 +4,10 @@ from app import create_app
 def test_home_page():
     """
     describe this please
+    this will not work because of the database
     """
 
     flask_app = create_app('flask_test.cfg')
     with flask_app.test_client() as test_client:
-        response = test_client.get('/home')
+        response = test_client.get('home')
         assert response.status_code == 308
